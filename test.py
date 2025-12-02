8a5681b2355e424a37c9c83ca5b78ac1fd34e261
jonaw2005 <jona@wwwinkler.de>
2025-12-03 00:47:00 +0100

    a

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ mac = "00:1A:2B:3C:4D:5E"
 serial = "SN987654"
 time = "2024-06-01T12:00:00Z"
 
-
+print(requests.post("http://localhost:5000/entriegeln", json={
+    "serial_number": serial}).json())
 
 requests.post("http://localhost:5000/register", json={
     "mac_address": mac, "serial_number": serial})
